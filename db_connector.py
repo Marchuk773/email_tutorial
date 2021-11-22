be89b4b19e253e746e2b27f7eb6a67ec28513413
Marchuk Roman <marchukroman228@gmail.com>
2021-11-22 17:51:10 +0200

    Dockerized application, add functionality to view and download attachments

--- db_connector.py
+++ db_connector.py
@@ -1,6 +1,6 @@
 import os
-
 import re
+
 from dotenv import load_dotenv
 from mysql import connector
 
@@ -13,9 +13,10 @@ def _get_value_from_brackets(line):
 
 
 class MysqlConnector:
-    def __init__(self):
+    def __init__(self, host=None, port=None):
         self.connection = connector.connect(
-            host=os.getenv('MYSQL_HOST'),
+            host=host or os.getenv('MYSQL_HOST'),
+            port=port or os.getenv('MYSQL_PORT'),
             database=os.getenv('MYSQL_DB'),
             user=os.getenv('MYSQL_USER'),
             password=os.getenv('MYSQL_PASSWORD'))
@@ -30,26 +31,85 @@ class MysqlConnector:
     def delete_email_by_id(self, email_id):
         query = 'DELETE FROM email WHERE id=%s;'
         with self.connection.cursor() as cursor:
-            cursor.execute(query, tuple(email_id))
+            cursor.execute(query, (email_id, ))
 
     def fetch_email_by_id(self, email_id):
         query = 'SELECT * FROM email WHERE id = %s;'
         with self.connection.cursor() as cursor:
-            cursor.execute(query, tuple(email_id))
+            cursor.execute(query, (email_id, ))
             result = cursor.fetchone()
-        return result
+        return {
+            'email_id': result[1],
+            'filepath': result[5]
+        } if result else None
 
-    def insert_email(self, email):
-        query = 'INSERT INTO email(email_id, msg_from, msg_to, subject) VALUES (%s, %s, %s, %s);'
-        values = (
-            str(_get_value_from_brackets(email['Message-Id'])),
-            str(_get_value_from_brackets(email['From'])),
-            str(_get_value_from_brackets(email['To'])),
-            str(email['Subject'].strip()),
-        )
+    def fetch_attachments_by_email_id(self, email_id):
+        query = 'SELECT * FROM attachment WHERE email_id = %s;'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (email_id, ))
+            result = cursor.fetchall()
+        return [{
+            'id': attachment[0],
+            'email_id': attachment[1],
+            'filepath': attachment[2],
+            'filename': attachment[2].split('/')[-1]
+        } for attachment in result] if result else None
+
+    def fetch_attachments_by_id(self, email_id):
+        query = 'SELECT * FROM attachment WHERE id = %s;'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, (email_id, ))
+            result = cursor.fetchone()
+        return {
+            'id': result[0],
+            'email_id': result[1],
+            'filepath': result[2],
+            'filename': result[2].split('/')[-1]
+        } if result else None
+
+    def insert_email(self, email, parsed_email_path):
+        query = ('INSERT INTO email(email_id, msg_from, msg_to, subject, ' +
+                 'parsed_email_path) VALUES (%s, %s, %s, %s, %s);')
+
+        values = (str(_get_value_from_brackets(email['Message-Id'])),
+                  str(_get_value_from_brackets(email['From'])),
+                  str(_get_value_from_brackets(email['To'])),
+                  str(email['Subject'].strip()), parsed_email_path)
+
         with self.connection.cursor() as cursor:
             cursor.execute(query, values)
         self.connection.commit()
+
+    def insert_attachment(self, email, attachment_path):
+        query = ('INSERT INTO attachment(email_id, path) ' +
+                 'VALUES (%s, %s);')
+        values = (_get_value_from_brackets(email['Message-Id']),
+                  attachment_path)
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, values)
+
+    def create_db(self):
+        email_query = """
+        CREATE TABLE IF NOT EXISTS email (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            email_id VARCHAR(120) UNIQUE NOT NULL,
+            msg_from VARCHAR(120),
+            msg_to VARCHAR(120),
+            subject VARCHAR(120),
+            parsed_email_path VARCHAR(120)
+        );
+        """
+        attachment_query = """
+        CREATE TABLE IF NOT EXISTS attachment (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            email_id VARCHAR(120),
+            path varchar(120),
+            FOREIGN KEY (email_id) REFERENCES email(email_id)
+        );
+        """
+        with self.connection.cursor() as cursor:
+            cursor.execute(email_query)
+            cursor.execute(attachment_query)
 
     def create_views(self):
         query = """
--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 from email.parser import BytesParser
 from email.policy import default
 from glob import glob
@@ -12,19 +13,39 @@ def parse_email(file):
         return BytesParser(policy=default).parse(f)
 
 
+def get_attachments_name(attachment):
+    match = re.search(r'name=".*"', attachment)
+    return match.group(0)[6:-1] if match else attachment
+
+
+def store_file(email_body, output_dir):
+    with open(output_dir, 'wb') as f:
+        f.write(bytes(email_body))
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description='Specify input and output directories.')
+        description='Specify directories for input, output and attachments.')
 
-    parser.add_argument('-i', '--input', default='./emails')
+    parser.add_argument('-i', '--input', default='emails')
+    parser.add_argument('-o', '--output', default='parsed_emails')
+    parser.add_argument('-a', '--attachments', default='attachments')
     args = parser.parse_args()
 
-    connector = db_connector.MysqlConnector()
-    email_files = glob(args.input + '/*.eml')
+    connector = db_connector.MysqlConnector(host="localhost", port=3307)
+    email_files = glob(f'./{args.input}/*.eml')
 
     for file in email_files:
         parsed_email = parse_email(file)
-        connector.insert_email(parsed_email)
+        new_path = f'./{args.output}{file[len(args.input) + 2:-4]}'
+        connector.insert_email(parsed_email, new_path)
+        store_file(bytes(parsed_email.get_body()), new_path)
+        for attachment in parsed_email.iter_attachments():
+            filename = get_attachments_name(attachment['Content-Type'])
+            attachment_path = f'./static/{args.attachments}/{filename}'
+            store_file(attachment.get_payload(decode=True), attachment_path)
+            connector.insert_attachment(parsed_email, attachment_path)
+
         os.rename(file, file + ".bkp")
 
 
